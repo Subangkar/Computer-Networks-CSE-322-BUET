c14731e6c1cb9d2419a4916900a601da2162dd0c
Subangkar <subangkar.karmaker@gmail.com>
2018-11-04 12:41:54 +0600

    Missing File Detection Done

--- Assignment01/HTTPSocket/src/Socket/HTTP/Server/ClientManager.java
+++ Assignment01/HTTPSocket/src/Socket/HTTP/Server/ClientManager.java
@@ -4,11 +4,13 @@ import IO.FileIOManager;
 import IO.InputReader;
 import IO.OutputWriter;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.StringTokenizer;
 
 /**
  * Created by Subangkar on 03-Nov-18.
@@ -18,6 +20,14 @@ public class ClientManager implements Runnable {
 	private String name;
 	private Socket client;
 	
+	private OutputWriter out;
+	
+	private final String NOT_FOUND = "<html>\n" + "<head><title>404 Not Found</title></head>\n" +
+			                                 "<body bgcolor=\"white\">\n" +
+			                                 "<center><h1>404 Not Found</h1></center>\n" +
+			                                 "<hr><center>:/</center>\n" +
+			                                 "</body>\n" +
+			                                 "</html>";
 	
 	public ClientManager( String name , Socket client ) {
 		this.name = name;
@@ -38,18 +48,32 @@ public class ClientManager implements Runnable {
 	public void run() {
 		try {
 			InputReader in = new InputReader( this.client.getInputStream() );
-			OutputWriter out = new OutputWriter( this.client.getOutputStream() );
-			String input = in.readNextLine();
-			writeLog( "Here Input : " + input );
+			out = new OutputWriter( this.client.getOutputStream() );
+			var input = in.readNextLine();
+			writeLog( "New Request line: " + input );
 			
-			out.writeLine( "HTTP/1.1 200 OK" );
-			out.writeLine( "Content-Type: text/html" );
-			out.writeLine( "Connection: close" );
-			out.writeLine();
+			if (input.isEmpty() || input.isBlank()) {
+				INVALID_Handler();
+			} else {
+				StringTokenizer stk;
+				stk = new StringTokenizer( input , " " );
+				String req, path, httpType;
+				req = stk.nextToken();
+				path = stk.nextToken();
+				httpType = stk.nextToken();
+				
+				if (!httpType.equalsIgnoreCase( "HTTP/1.1" )) {
+					INVALID_Handler();
+				}
+				else if (req.equalsIgnoreCase( "GET" )) {
+					GET_Handler( path );
+				} else if (req.equalsIgnoreCase( "POST" )) {
+					POST_Handler( path );
+				} else {
+					INVALID_Handler();
+				}
+			}
 			
-			out.write( FileIOManager.readFileToCharString( "index.html" , StandardCharsets.UTF_8 ) );
-			writeLog( "Terminating Connection" );
-			client.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -57,6 +81,44 @@ public class ClientManager implements Runnable {
 	
 	
 	private void writeLog( String log ) {
-		System.out.println(  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime()) + " >> " + client.getInetAddress().getHostAddress() + ":" + client.getPort() + " >> " + log );
+		System.out.println( new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" ).format( Calendar.getInstance().getTime() ) + " >> " + client.getInetAddress().getHostAddress() + ":" + client.getPort() + " >> " + log );
+	}
+	
+	
+	private void GET_Handler( String path ) throws IOException {
+		
+		if (path.equalsIgnoreCase( "/" )) path = "/index.html";
+		
+		File file = new File( path.substring( 1 ) );
+		
+		if (!file.exists()) {
+			writeLog( path + " File Not Found in Directory" );
+			out.writeLine( "HTTP/1.1 404 NOT FOUND" );
+			out.writeLine();
+			out.writeLine( NOT_FOUND );
+		} else {
+			writeLog( "Requested file: " + file.getPath() );
+			
+			out.writeLine( "HTTP/1.1 200 OK" );
+			out.writeLine( "Content-Type: text/html" );
+			out.writeLine( "Connection: close" );
+			out.writeLine();
+			
+			out.write( FileIOManager.readFileToCharString( file.getPath() , StandardCharsets.UTF_8 ) );
+		}
+		writeLog( "Terminating Connection" );
+		client.close();
+	}
+	
+	private void POST_Handler( String path ) {
+	
+	}
+	
+	private void INVALID_Handler() throws IOException {
+		writeLog( "Invalid request line without GET/POST" );
+		out.writeLine( "HTTP/1.1 400 BAD REQUEST" );
+		out.writeLine( "Connection: close" );
+		out.writeLine();
+		client.close();
 	}
 }
