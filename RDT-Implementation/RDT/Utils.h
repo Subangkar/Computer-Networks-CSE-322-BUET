6f125257defe270a89c5f1c66bc7870c80a5bbc3
Subangkar-GitKraken-Win10-HP <subangkar.karmaker@gmail.com>
2019-01-28 18:40:21 +0600

    RDT3.0 ABP and GBN implementation

---
+++ RDT-Implementation/RDT/Utils.h
@@ -0,0 +1,51 @@
+//
+// Created by Subangkar on 25-Jan-19.
+//
+
+#ifndef RDT_UTILS_H
+#define RDT_UTILS_H
+
+//#define ABP_SIM
+//#define GBN_SIM
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cstdio>
+
+//#define DEBUG
+//#define DEBUG_ABP
+//#define DEBUG_GBN
+
+#define DEBUG_NMSG 20
+#define DEBUG_PROB_LOSS 0.20//0.05
+#define DEBUG_PROB_CORP 0.20//0.45
+#define DEBUG_TIME 100
+#define DEBUG_TRACE 2
+
+///======================= Data Defn ===================
+
+#define MSG_LEN 20
+
+
+#define ACK_ABP_DEFAULT 0x0F
+#define SEQ_ABP_DEFAULT 0x0F
+#define ACK_GBN_DEFAULT 0x0F
+
+int nDroppedMessages;
+
+///=============================================================
+
+/// simple sum of all
+int calc_checksum(int seqnum, int acknum, const char payload[MSG_LEN]) {
+	int i;
+	int checksum = 0;
+	for (i = 0; i < MSG_LEN; i++) { // check till NULL
+		checksum += (unsigned char) payload[i];
+	}
+	checksum += seqnum;
+	checksum += acknum;
+	return checksum;
+}
+
+#endif //RDT_UTILS_H
